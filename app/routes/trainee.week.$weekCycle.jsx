4f62e2474366c8f7b92bd269af7739588844665c
Robertino Pomper <robertino@binaria.io>
2024-07-11 06:56:45 +0200

    Update trainee.week.$weekCycle.jsx

--- app/routes/trainee.week.$weekCycle.jsx
+++ app/routes/trainee.week.$weekCycle.jsx
@@ -2,9 +2,7 @@ import { useLocation } from "@remix-run/react";
 import WeeklyReport from "~/components/trainee-specific/WeeklyReport";
 
 const WeeklyReportPage = () => {
-  //   const weeklyCycle = useLocation().params.weekCycle;
   const location = useLocation();
-  console.log(location);
   const weeklyCycle = location.pathname.split("/").pop();
   return <WeeklyReport weeklyCycle={weeklyCycle} />;
 };
